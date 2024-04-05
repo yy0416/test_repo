5e8fa4e1c7f5d4f6598074cf30d284e68b90f0ed
yuan <wheathare@gmail.com>
2024-04-05 10:29:06 +0200

    04

--- 04_arrow.js
+++ 04_arrow.js
@@ -7,7 +7,8 @@
  *   - le mot clée "function" est interdis
  */
 
-let arrow1 = () => {}
+let arrow1 = (a) => {return a+1;}
+console.log(arrow1(2));
 
 /**
  * Déclarer une fonction fléchée qui renvoi l'argument + 1
@@ -18,6 +19,7 @@ let arrow1 = () => {}
  *   - le mot clée "return" est interdis
  */
 
-let arrow2 = () => {}
+let arrow2 = b => b+1;
+console.log(arrow2(5));
 
 module.exports = {arrow1, arrow2};
